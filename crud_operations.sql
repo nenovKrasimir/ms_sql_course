3f4a6c696c8382b4b7c6e1119cefd369b0fec39b
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-10-01 13:45:41 +0300

    Create crud_operations.sql

---
+++ crud_operations.sql
@@ -0,0 +1,15 @@
+-- 2. Find All the Information About Departments
+
+-- Create a SQL query that finds all the available information about the Departments.
+
+-- Example
+
+-- DepartmentID Name ManagerID
+
+-- 1 Engineering 12
+
+-- 2 Tool Design 4
+
+-- 3 Sales 273
+
+-- … … …
